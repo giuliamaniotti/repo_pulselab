9aad0ceacced00e86b9aacfda08872db8f28c797
DelfinaFC <dfernandezcovaro@udesa.edu.ar>
2026-04-05 21:19:50 -0300

    Update procesamiento_datos.py
    funcion filtrar_por_participante

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -6,3 +6,21 @@ Created on Sun Apr  5 20:46:42 2026
 @author: giuliamaniotti
 """
 
+def filtrar_por_participante(datos: list, id_participante: int) -> dict:
+    """
+    Qué hace la función:
+    Selecciona y devuelve los datos correspondientes a un participante.
+
+    Parámetros:
+    - datos: lista de diccionarios con los registros de participantes
+    - id_participante: identificador del participante a buscar
+
+    Retorna:
+    - dict: datos del participante encontrado
+    - None: si no se encuentra el participante
+    """
+    for participante in datos:
+        if participante["id_participante"] == id_participante:
+            return participante
+
+    return None
